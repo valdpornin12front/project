9e20a154e1c5f39005197e279e66402cb9ceed55
k58324762t <wirdjsgt@gmail.com>
2022-07-06 12:23:59 +0600

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import {favoriteReducer} from "./favoriteReducer";
+
+export const rootReducer = combineReducers({
+    favoriteReducer:favoriteReducer
+
+});
+
+
+
+
